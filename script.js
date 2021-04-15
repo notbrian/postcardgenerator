e200ceb240daf1babcf4d94b9c3e356c935392a7
Brian Nguyen <nguyen.brian70@gmail.com>
2021-04-15 00:06:22 -0400

    Add basic face masking

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let segmentation;
 let img;
 
 const options = {
-  multiplier: 0.75, // 1.0, 0.75, or 0.50, 0.25
+  multiplier: 0.5, // 1.0, 0.75, or 0.50, 0.25
   outputStride: 8, // 8, 16, or 32, default is 16
-  segmentationThreshold: 0.2, // 0 - 1, defaults to 0.5
+  segmentationThreshold: 0.8, // 0 - 1, defaults to 0.5
   //   palette: leftFace,
 };
 
@@ -35,7 +35,7 @@ function videoReady() {
 function draw() {
   background(0);
   if (segmentation) {
-    image(segmentation.partMask, 0, 0, width, height);
+    image(segmentation.personMask, 0, 0, width, height);
   }
 }
 
@@ -45,5 +45,28 @@ function gotResults(error, result) {
     return;
   }
   segmentation = result;
+  segmentation.partMask.loadPixels();
+  segmentation.personMask.loadPixels();
+
   console.log(segmentation);
+
+  let leftFaceColor = JSON.stringify(segmentation.bodyParts.leftFace.color);
+  let rightFaceColor = JSON.stringify(segmentation.bodyParts.rightFace.color);
+
+  for (let i = 0; i < segmentation.personMask.pixels.length; i += 4) {
+    let maskColor = JSON.stringify([
+      segmentation.partMask.pixels[i],
+      segmentation.partMask.pixels[i + 1],
+      segmentation.partMask.pixels[i + 2],
+    ]);
+    // Compare pixel colors from partMask
+    if (maskColor !== leftFaceColor && maskColor !== rightFaceColor) {
+      // segmentation.personMask.pixels[i]
+      // segmentation.personMask.pixels[i + 1]
+      // segmentation.personMask.pixels[i + 2]
+      segmentation.personMask.pixels[i + 3] = 0;
+    }
+  }
+
+  segmentation.personMask.updatePixels();
 }
